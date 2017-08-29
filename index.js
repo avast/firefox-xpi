c805d1186d6fa2997af7909cf1a2c5bf504ddfe4
Pavel Studeny <studeny@avast.com>
2017-08-29 15:53:02 +0200

    web extension support (just build, signing not included)

--- index.js
+++ index.js
@@ -10,7 +10,6 @@
     var when = require('when');
     var path = require('path');
     var fs = require('fs');
-    var xpi = require('jpm/lib/xpi');
 
     // pack Firefox extension
     //
@@ -19,12 +18,59 @@
     // @return promise
     // 
     function pack(xpiName, addonDir, options) {
+        try {
+            var webExtManifest = path.join(addonDir, 'manifest.json');
+            fs.accessSync(webExtManifest);
+            try {
+                return packWebExt(xpiName, addonDir, options);
+            }
+            catch (ex) {
+                return Promise.reject(ex);
+            }
+        }
+        catch (ex) {
+            return packAddon(path.resolve(xpiName), path.resolve(addonDir));
+        }
+    }
+
+    function packAddon(xpiName, addonDir) {
+        var xpi = require('jpm/lib/xpi');
         var manifest = require(path.join(addonDir, 'package.json'));
-        console.log("xpi ", addonDir, " ", path.dirname(xpiName), " ", path.basename(xpiName))
+        console.log("xpi ", addonDir, " ", path.dirname(xpiName), " ", path.basename(xpiName));
         return xpi(manifest, { addonDir: addonDir, xpiPath: path.dirname(xpiName), xpiName: path.basename(xpiName) })  // , verbose: true
             .then(function (fullPath) {
                 fs.renameSync(fullPath, xpiName);
                 return 0;
+            });
+    }
+
+    function packWebExt(zipName, addonDir, options) {
+        var myArgv = process.argv.slice();
+        var webExtModulePath = path.resolve(require.resolve('web-ext/dist/web-ext.js'), '../..');
+        var intermediateDir = options && options.tmpDir ? path.resolve(options.tmpDir, 'web-ext-artifacts') : path.resolve(path.dirname(zipName), 'web-ext-artifacts');
+
+        process.argv = [ process.argv[0], path.resolve(webExtModulePath, 'bin/web-ext'), 'build', '--overwrite-dest', '--artifacts-dir=' +  intermediateDir];
+
+        var rm = require('del').sync;
+        rm(intermediateDir, {force: true});
+        
+        var wd = process.cwd();
+        process.chdir(addonDir);
+
+        var webExt = require('web-ext/dist/web-ext.js').main;
+        return webExt(webExtModulePath)
+            .then(function () {
+                process.chdir(wd);
+                var result = fs.readdirSync(intermediateDir)[0];
+                fs.renameSync(path.resolve(intermediateDir, result), zipName);
+                rm(intermediateDir, {force: true});
+                console.log("WebExtension at " + zipName);
+                return 0;
+            })
+            .catch(function (ex) {
+                console.log(ex);
+                process.chdir(wd);
+                return 1;
             });
     }
 
